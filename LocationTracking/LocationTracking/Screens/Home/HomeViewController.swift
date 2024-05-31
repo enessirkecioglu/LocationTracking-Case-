12a626299b51f1b7eb2f0650232459b87e2dfe8c
enessirkecioglu <enes.sirkecioglu1@gmail.com>
2024-05-31 17:44:11 +0300

    feat: create file structure

---
+++ LocationTracking/LocationTracking/Screens/Home/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewController.swift
+//  LocationTracking
+//
+//  Created by Enes Sirkecioğlu on 31.05.2024.
+//
+
+import UIKit
+
+final class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+}
---
+++ LocationTracking/LocationTracking/Screens/Splash/SplashViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SplashViewController.swift
+//  LocationTracking
+//
+//  Created by Enes Sirkecioğlu on 31.05.2024.
+//
+
+import UIKit
+
+final class SplashViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+}
